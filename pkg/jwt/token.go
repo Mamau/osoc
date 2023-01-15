4dc1062cc88a158d30454e4f87418839ab503c82
mamau <mopckou_pak48rus@mail.ru>
2023-01-16 00:44:15 +0200

    change route to gin

--- pkg/jwt/token.go
+++ pkg/jwt/token.go
@@ -39,19 +39,6 @@ func CreateRefreshToken(id int, secret string) (refreshToken string, err error) 
 	return rt, err
 }
 
-func IsAuthorized(requestToken string, secret string) (bool, error) {
-	_, err := jwt.Parse(requestToken, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(secret), nil
-	})
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
 func ExtractIDFromToken(requestToken string, secret string) (int, error) {
 	token, err := jwt.ParseWithClaims(requestToken, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
